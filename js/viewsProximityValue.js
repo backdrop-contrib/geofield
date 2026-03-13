5fdba26cd963c9d442ac37a274684641edddfad1
indigoxela <user@olga.indigofloat.at>
2026-03-13 13:57:52 +0100

    Issue #62: Get dynamic form update working for single filter

--- js/viewsProximityValue.js
+++ js/viewsProximityValue.js
@@ -1,12 +1,72 @@
-;(function ($) {
+/**
+ * @file
+ * Dynamically update Views admin UI form parts by sending POST.
+ */
+(function ($) {
+
+  "use strict";
+
   Backdrop.behaviors.viewsProximityValue = {
-    attach: function (context, settings) {
-      if (!$('body').hasClass('page-admin-structure-views-nojs')) {
-        $('#edit-options-source-change').hide();
+    /**
+     * @param array data
+     */
+    updateFormItem: function (data) {
+      let formItemsWrapped = data[1].data;
+      let formItems = $(formItemsWrapped).find('.geofield-proximity-field-wrapper');
+      if (formItems.length) {
+        $('.geofield-proximity-field-wrapper').replaceWith(formItems);
       }
-      $('#edit-options-source').change(function() {
-        $('#edit-options-source-change').mousedown();
-        $('#edit-options-source-change').submit();
+    },
+    /**
+     * @return bool
+     */
+    formHasRange: function () {
+      let checkedValue = $('#edit-options-operator').find(':checked').val();
+      if (checkedValue === 'between' || checkedValue === 'not between') {
+        return true;
+      }
+      return false;
+    },
+    /**
+     *
+     */
+    attach: function (context, settings) {
+      if ($('#edit-options-group-button-radios :checked').val() === 1) {
+        // This behavior does not work with grouped exposed filters.
+        return;
+      }
+      const widget = this;
+      let hasRange = widget.formHasRange();
+      $.ajaxSetup({
+        type: 'POST',
+        url: $('#edit-options-source').data('path'),
+        dataType: 'json'
+      });
+
+      $('#edit-options-source').on('change', function() {
+        let postData = {
+          plugin: this.value
+        };
+        $.ajax( { data: postData } )
+          .done( function (data) {
+            widget.updateFormItem(data);
+        });
+      });
+
+      $('#edit-options-operator').on('change', function() {
+        // Only post if necessary.
+        if (hasRange === widget.formHasRange()) {
+          return;
+        }
+        let postData = {
+          plugin: $('#edit-options-source').val(),
+          operator: $(this).find(':checked').val()
+        };
+        $.ajax( { data: postData } )
+          .done( function (data) {
+            widget.updateFormItem(data);
+            hasRange = widget.formHasRange();
+        });
       });
     }
   };
